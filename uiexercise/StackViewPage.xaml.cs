fb9f8d67dc3aa2b369dd3f2adcc71f9d2e40c034
Max_Gabriel_Susman <gabe.susman@gmail.com>
2020-09-07 12:40:03 -0600

    completed listview exercise

---
+++ uiexercise/StackViewPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace uiexercise
+{
+    public partial class StackViewPage : ContentPage
+    {
+        public StackViewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
