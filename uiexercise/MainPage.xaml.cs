6cf4471e3acdbc340068ef9d1ed471962eae645f
Max_Gabriel_Susman <gabe.susman@gmail.com>
2020-09-08 12:42:27 -0600

    completed aciton sheet exercise

--- uiexercise/MainPage.xaml.cs
+++ uiexercise/MainPage.xaml.cs
@@ -22,5 +22,11 @@ namespace uiexercise
             bool response = await DisplayAlert("Save?", "Would you like to save your data?", "Yes", "No");
             Console.WriteLine("Save data: " + response);
         }
+
+        async void OnDisplayActionSheetButtonClicked(object sender, EventArgs e)
+        {
+            string action = await DisplayActionSheet("Send to?", "Cancel", null, "Email", "Twitter", "Facebook");
+            Console.WriteLine("Action: " + action);
+        }
     }
 }
